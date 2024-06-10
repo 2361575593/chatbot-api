0e1cc3f714ea5e816f07d6a4b48f519e47dedf9c
2361575593 <2361575593@qq.com>
2024-06-10 16:37:49 +0800

    va feat:init

---
+++ chatbot-api-interface/src/main/java/org/va/chatapi/ApiApplication.java
@@ -0,0 +1,17 @@
+package org.va.chatapi;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ApiApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class,args);
+    }
+}
